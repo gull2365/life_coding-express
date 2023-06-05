021d9fbf9b0f4237394d63a5aa47e799c7f3dc98
Gull 2365 <jych2365@gmail.com>
2023-06-05 22:38:57 +0900

    feat: Express - 3.2. Hello Word 2

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+//route, routing
+// app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", function (req, res) {
+  res.send("/");
+});
+
+app.get("/page", function (req, res) {
+  res.send("/page");
+});
 
 app.listen(3000, () => console.log("Example app listening on port 3000!"));
 // var http = require("http");
