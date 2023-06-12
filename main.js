7d00682fbfde07e7b34b833b2ebdc4a2c76b64ba
Gull 2365 <jych2365@gmail.com>
2023-06-12 13:00:21 +0900

    feat: Express - 5.2. 상세페이지 구현 2

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-const express = require("express");
-const fs = require("fs");
+var express = require("express");
+var app = express();
+var fs = require("fs");
+var path = require("path");
+var sanitizeHtml = require("sanitize-html");
 var template = require("./lib/template.js");
-const app = express();
 
 //route, routing
-// app.get("/", (req, res) => res.send("Hello World!"));
-app.get("/", function (req, res) {
+//app.get('/', (req, res) => res.send('Hello World!'))
+app.get("/", function (request, response) {
   fs.readdir("./data", function (error, filelist) {
     var title = "Welcome";
     var description = "Hello, Node.js";
@@ -16,15 +18,39 @@ app.get("/", function (req, res) {
       `<h2>${title}</h2>${description}`,
       `<a href="/create">create</a>`
     );
-    res.send(html);
+    response.send(html);
   });
 });
 
-app.get("/page/:pageId", function (req, res) {
-  res.send(req.params);
+app.get("/page/:pageId", function (request, response) {
+  fs.readdir("./data", function (error, filelist) {
+    var filteredId = path.parse(request.params.pageId).base;
+    fs.readFile(`data/${filteredId}`, "utf8", function (err, description) {
+      var title = request.params.pageId;
+      var sanitizedTitle = sanitizeHtml(title);
+      var sanitizedDescription = sanitizeHtml(description, {
+        allowedTags: ["h1"],
+      });
+      var list = template.list(filelist);
+      var html = template.HTML(
+        sanitizedTitle,
+        list,
+        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+        ` <a href="/create">create</a>
+          <a href="/update?id=${sanitizedTitle}">update</a>
+          <form action="delete_process" method="post">
+            <input type="hidden" name="id" value="${sanitizedTitle}">
+            <input type="submit" value="delete">
+          </form>`
+      );
+      response.send(html);
+    });
+  });
 });
 
-app.listen(3000, () => console.log("Example app listening on port 3000!"));
+app.listen(3000, function () {
+  console.log("Example app listening on port 3000!");
+});
 // var http = require("http");
 // var fs = require("fs");
 // var url = require("url");
